896bc9b4efeba4aa623acd91fb47b08ab2383411
Dimon07993 <totti69.93@gmail.com>
2024-03-19 19:07:48 +0300

    поправки вывода

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from utils.code import print_data
 s1 = print_data('data/operations.json')
 
 for i in s1:
-    print(i)
-    #if 'to' in i:
-    #    print(f'{i["date"]} {i["descr"]}')
-    #    print(f'{i["from"]} -> {i["to"]}')
-    #    print(f'{i["operationAmount"]["amount"]} {i["operationAmount"]["currency"]["name"]}' + '\n')
-    #else:
-    #    print(f'{i["date"]} {i["descr"]}')
-    #    print(f'{i["from"]}')
-    #    print(f'{i["operationAmount"]["amount"]} {i["operationAmount"]["currency"]["name"]}' + '\n')
-#
+    if 'to' in i:
+        print(f'{i["date"]} {i["descr"]}')
+        print(f'{i["from"]} -> {i["to"]}')
+        print(f'{i["operationAmount"]["amount"]} {i["operationAmount"]["currency"]["name"]}' + '\n')
+    else:
+        print(f'{i["date"]} {i["descr"]}')
+        print(f'{i["from"]}')
+        print(f'{i["operationAmount"]["amount"]} {i["operationAmount"]["currency"]["name"]}' + '\n')
